30a221e90db50a8aa1bfa04e63290f824a6395e2
litwinm <75579317+litwinm@users.noreply.github.com>
2020-12-08 15:53:20 +0100

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _fizzBuzz
+{
+    public class FizzBuzz
+    {
+        public void Execute(int number)
+        {
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+
+            }
+
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+
+            }
+
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+
+            }
+
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                var number = GetInput();
+                FizzBuzz fizzbuzz = new FizzBuzz();
+                fizzbuzz.Execute(number);
+
+            }
+            catch
+            {
+                Console.WriteLine("Wprowadzone dane są nieprawidłowe!");
+
+            }
+
+        }
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int value))
+                throw new Exception("Wprowadzone dane są nieprawidłowe!");
+
+            return value;
+        }
+    }
+}
